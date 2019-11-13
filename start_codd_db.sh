b438e751301e80d148863631ad7e6f54223ac801
chaudharyachint08 <chaudharyachint08@gmail.com>
2019-11-13 19:43:54 +0530

    mid-term report

---
+++ start_codd_db.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Starting postgres on data_directory
+/home/malhar/DB_exec/postgresql-9.4.1_CODD/bin/postgres -D /home/malhar/DB_exec/pg_data/
+
+
+#conda activate /home/achintc/anaconda3/envs/DSL
+#python3 /home/achintc/sample.py
+
+
+#conda /home/achintc/anaconda3/bin/conda deactivate
+
+#pwd; hostname; date | tee achintc/result.txt
