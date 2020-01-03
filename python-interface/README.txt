70e22e04e87889d6f883bad652d6caef4625401e
chaudharyachint08 <chaudharyachint08@gmail.com>
2020-01-03 18:21:14 +0530

    some

---
+++ python-interface/README.txt
@@ -0,0 +1,3 @@
+Queries in this folder are tested on TPC-H, while we will be working with TPC-DS or JOB further, those queries need to be changed as per the requirements of benchmark used
+
+Also, RealDictCursor is used to access via only column names, not index number & on JSON files it will give Keys which is not with DictCursor
---
+++ python-interface/fetch_xml.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+try:
+    connection = psycopg2.connect(user = "sa",
+                                  password = "database",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "tpcds-1")
+    #cursor = connection.cursor(cursor_factory=RealDictCursor)
+    cursor = connection.cursor()
+    #Query to be executed with format & other specifiers
+    query = '''EXPLAIN (COSTS, VERBOSE, FORMAT XML) select * from call_center;'''
+    cursor.execute(query)
+    f = open('output.xml','w')
+    f.write(cursor.fetchone()[0])
+    f.close()
+
+except (Exception, psycopg2.DatabaseError) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+    #closing database connection.
+    if(connection):
+        cursor.close()
+        connection.close()
+        print("PostgreSQL connection is closed")
