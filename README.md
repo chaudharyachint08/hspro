c204f0d9a35e7f86a27dfe987322058ee1b79783
chaudharyachint08 <chaudharyachint08@gmail.com>
2020-01-04 18:28:07 +0530

    Class based implementation begin

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 
 <pre>
-	NEXUS : Get All Plans on Each of Iso-cost surfaces
-	Anorexic Reduction : Reducing plan density on each surface
-	Sequence Covering Code : Further Reduction of plan density on each surface
+	1. NEXUS : Get All Plans on Each of Iso-cost surfaces
+	2. Anorexic Reduction : Plan swallowing for reducing plan density on each surface
+	3. Covering sequence : Further Reduction of plan density on each surface, (check if impacts ASO)
+	4. Execution of bouquet, or simulation of execution under ideal cost model assumption
 </pre>
--- source/main.py
+++ source/main.py
@@ -23,11 +23,11 @@ os.environ['KERAS_BACKEND'] = 'tensorflow'
 
 # Importing Standard Data Science & Deep Learning Libraries
 ls = [('matplotlib','mpl'),('matplotlib.pyplot','plt'),('seaborn','sns'),
-	('numpy','np'),'scipy',	'sklearn',
-	('tensorflow','tf'),'h5py','keras']
+    ('numpy','np'),'scipy',    'sklearn',
+    ('tensorflow','tf'),'h5py','keras']
 for i in ls:
-	if not isinstance(i,tuple):
-		i = (i,)
+    if not isinstance(i,tuple):
+        i = (i,)
     exec('import {0} as {1}'.format(i[0],i[-1]))
     exec('print("Version of {0}",{1}.__version__)'.format(i[0],i[-1]))
 
@@ -35,17 +35,22 @@ for i in ls:
 
 
 
-######## GLOBAL INITIAL VARIABLES BEGINS ########
+######## COMMAND LINE ARGUMENTS BEGINS ########
 
 parser = argparse.ArgumentParser()
 
 # Bool Type Arguments
-parser.add_argument("--zero_sel"   , type=eval , dest='zero_sel'   , default=False)
-parser.add_argument("--new_plans"  , type=eval , dest='new_plans'  , default=False)
+parser.add_argument("--zero_sel" , type=eval , dest='zero_sel' , default=False)
+parser.add_argument("--new_plan" , type=eval , dest='new_plan' , default=False)
+parser.add_argument("--anorexic" , type=eval , dest='anorexic' , default=False)
+parser.add_argument("--covering" , type=eval , dest='covering' , default=False)
+
 # Int Type Arguments
-parser.add_argument("--resolution" , type=eval , dest='resolution' , default=10)
+parser.add_argument("--resolution" , type=eval , dest='resolution' , default=1000)
+parser.add_argument("--base_scale" , type=eval , dest='base_scale' , default=1)
 parser.add_argument("--sel_round"  , type=eval , dest='sel_round'  , default=None)
 # Float Type Arguments
+parser.add_argument("--r_ratio"         , type=eval , dest='r_ratio'         , default=2.0) # IC cost ratio for bouquet
 parser.add_argument("--min_sel"         , type=eval , dest='min_sel'         , default=0.0001) # Least sel out of 1
 parser.add_argument("--max_sel"         , type=eval , dest='max_sel'         , default=1.0) # Maximum sel of 1
 parser.add_argument("--anorexic_lambda" , type=eval , dest='anorexic_lambda' , default=0.2) # Cost Slack
@@ -55,102 +60,158 @@ parser.add_argument("--progression" , type=str  , dest='progression' , default='
 parser.add_argument("--benchmark"   , type=str  , dest='benchmark'   , default='tpcds')
 parser.add_argument("--master_dir"  , type=str  , dest='master_dir'  , default=os.path.join('.','..','experiments','bouquet_master' ))
 parser.add_argument("--plots_dir"   , type=str  , dest='plots_dir'   , default=os.path.join('.','..','experiments','bouquet_plots'))
+# Tuple Type Arguments
+parser.add_argument("--db_scales" , type=eval , dest='db_scales' , default=(1,2,5,10,20,30,40,50,75,100,125,150,200,250))
 
 args, unknown = parser.parse_known_args()
 globals().update(args.__dict__)
 
-plan_map = {} # (query, sel)       : PLAN_ID  , Getting ID of optimal plan at given Selectivity
-fpc_map  = {} # (query, plan, sel) : COST_VAL , FPC outputs, Evaluates a Plan at Selectivity value
-
-IC2plan_map = {} # (query,IC)   : LIST_PLANS , list of plans on iso-cost surface
-plan2IC_map = {} # (query,plan) : CONTOUR_ID , iso-cost contour id of any plan
-IC2cost_map = {} # (query,IC)   : Cost budget of iso-cost surface
-
-######## GLOBAL INITIAL VARIABLES ENDS ########
+######## COMMAND LINE ARGUMENTS ENDS ########
 
 
+######## GLOBAL DATA-STRUCTURES BEGINS ########
 
+pwd = os.getcwd()
 
-######## PERFORMANCE METRICS BEGINS ########
+######## GLOBAL DATA-STRUCTURES ENDS ########
 
 
 eval( json["QUERY PLAN"][0]["Plan"]["Startup-Cost"] )
 eval( json["QUERY PLAN"][0]["Plan"]["Total-Cost"] )
 
-def cost(query, plan, sel):
-	"Costs plan at some selectivity value"
-	if (query, plan, sel) not in fpc_map:
-		'# Code for FPC call to cost plan'
+
+class ScaleVariablePlanBouquet:
+	''
+	'''
+	'''
+
+	def __init__(self,benchmark,query,base_scale,db_scales):
+		self.benchmark, self.query, self.base_scale, self.db_scales = benchmark, query, base_scale, db_scales
+		self.struct2file_map = {} # (serial_plan) : Plan_Structure
+		self.file2plan_map   = {} # (plan_file_name)   : provides plan_if for given plan file name, without extension
+		self.plan2file_map   = {} # (plan_id)     : provides plan_if for given plan path
+
+		self.sel2plan_map    = {} # (sel) : Getting ID of optimal plan at given selectivity
+		self.fpc_map  = {} # (plan, sel) : COST_VAL of some plan at some selectivity
+
+		IC2plan_map = {} # (benchmark,query,IC)   : LIST_PLANS , list of plans on iso-cost surface
+		plan2IC_map = {} # (benchmark,query,plan) : CONTOUR_ID , iso-cost contour id of any plan
+		IC2cost_map = {} # (benchmark,query,IC)   : Cost budget of iso-cost surface
+
+	def plan_serial(plan_path):
+		"Function to convert info providing plan ID to a string"
 		pass
-	return fpc_map[ (query, plan, sel) ]
 
-def SubOpt(query, est_sel, act_sel, bouquet=False):
-	"Ratio of plan on estimated sel to plan on actual sel"
-	act_plan = plan_map[ (query, act_sel) ]
-	if not bouquet:
-		'Classic Optimizer Style Metric'
-		est_plan = plan_map[ (query, est_sel) ]
-		return cost(query, est_plan, act_sel) / cost(query, act_plan, act_sel)
-	else:
-		'Bouquet Style Metric'
-		plan_list = ExecPlanBouquet(query, act_sel)
-		bouquet_cost = 0
-		for plan in plan_list[:-1]: # Since up-to second last plan, budget based execution
-			bouquet_cost += IC2cost_map[ (query, plan2IC_map[ (query,plan) ] ) ]
-		bouquet_cost += cost(query, est_plan, act_sel)
-		return bouquet_cost / cost(query, act_plan, act_sel)
-		pass
+	######## PERFORMANCE METRICS BEGINS ########
 
-def WorstSubOpt(query, act_sel):
-	"SubOpt for all possible est_selectivities"
-	return max( SubOpt(query, est_sel, act_sel) for est_sel in sel_range)
+	def cost(plan, sel):
+	    "Costs plan at some selectivity value"
+	    if (query, plan, sel) not in fpc_map:
+	        '# Code for FPC call to cost plan'
+	        pass
+	    return fpc_map[ (query, plan, sel) ]
 
-def MaxSubOpt(query):
-	"Global worst case"
-	return max( WorstSubOpt(query, act_sel) for act_sel in sel_range)
+	def SubOpt(benchmark, query, est_sel, act_sel, bouquet=False):
+	    "Ratio of plan on estimated sel to plan on actual sel"
+	    act_plan = plan_map[ (query, act_sel) ]
+	    if not bouquet:
+	        'Classic Optimizer Style Metric'
+	        est_plan = plan_map[ (query, est_sel) ]
+	        return cost(query, est_plan, act_sel) / cost(query, act_plan, act_sel)
+	    else:
+	        'Bouquet Style Metric'
+	        plan_list = ExecPlanBouquet(query, act_sel)
+	        bouquet_cost = 0
+	        for plan in plan_list[:-1]: # Since up-to second last plan, budget based execution
+	            bouquet_cost += IC2cost_map[ (query, plan2IC_map[ (query,plan) ] ) ]
+	        bouquet_cost += cost(query, est_plan, act_sel)
+	        return bouquet_cost / cost(query, act_plan, act_sel)
+	        pass
 
-def AvgSubOpt(query):
-	"Average suboptimality over ESS under uniformity assumption"
-	return sum( (SubOpt(query, est_sel, act_sel) for est_sel in sel_range for act_sel in sel_range) ) / (len(sel_range)**2)
+	def WorstSubOpt(query, act_sel):
+	    "SubOpt for all possible est_selectivities"
+	    return max( SubOpt(query, est_sel, act_sel) for est_sel in sel_range)
 
+	def MaxSubOpt(query):
+	    "Global worst case"
+	    return max( WorstSubOpt(query, act_sel) for act_sel in sel_range)
 
-def MaxHarm(query):
-	"Harm using Plan Bouquet, over Classic Optimizer"
-	return max( (SubOpt(query,est_sel=-1,act_sel,bouquet=True)/WorstSubOpt(query, act_sel)) for act_sel in sel_range ) - 1
+	def AvgSubOpt(query):
+	    "Average suboptimality over ESS under uniformity assumption"
+	    return sum( (SubOpt(query, est_sel, act_sel) for est_sel in sel_range for act_sel in sel_range) ) / (len(sel_range)**2)
 
-######## PERFORMANCE METRICS BEGINS ########
+	def MaxHarm(query):
+	    "Harm using Plan Bouquet, over Classic Optimizer"
+	    return max( (SubOpt(query,est_sel=-1,act_sel,bouquet=True)/WorstSubOpt(query, act_sel)) for act_sel in sel_range ) - 1
 
+	def cover(sel_1, sel_2):
+	    "Check if one region covers other"
+	    ls_1, ls_2 = np.array(sel_1), np.array(sel_2)
+	    if   (ls_1 <= ls_2).all(): # ls_1 is covered by ls_2
+	        return 1
+	    elif (ls_1 >= ls_2).all(): # ls_1 covers ls_2
+	        return -1
+	    else:
+	        return 0
 
+	def anorexic_reduction(query):
+	    pass
 
-
-######## PLAN BOUQUET BEGINS ########
-
-def cover(sel_1, sel_2):
-	"Check if one region covers other":
-	ls_1, ls_2 = np.array(sel_1), np.array(sel_2)
-	if   (ls_1 <= ls_2).all(): # ls_1 is covered by ls_2
-		return 1
-	elif (ls_1 >= ls_2).all(): # ls_1 covers ls_2
-		return -1
-	else:
-		return 0
-
-def anorexic_reduction(query):
-	pass
-
-def covering_sequence(query):
-	pass
+	def covering_sequence(query):
+	    pass
 
 
 ######## PLAN BOUQUET ENDS ########
 
+if not os.path.isdir(master_dir):
+    os.makedirs(master_dir)
+if not os.path.isdir(plots_dir):
+    os.makedirs(plots_dir)
 
 
-if __name__=='__main__':
-	sel_ratio = np.exp( np.log(max_sel/min_sel) / (resolution-1) )
-	sel_range = [(min_sel*sel_ratio**i) for i in range(resolution)]
-	if zero_sel:
-		sel_range.insert( 0, 0 )
-	sel_range = np.array( sel_range )
-	if sel_round is not None:
-		sel_range = np.round( sel_range, sel_round )
+
+if __name__=='__fuck__':
+
+    # Creating values of selectivities on each axis to be varied
+    if progression=='GP':
+        if zero_sel:
+            resolution -= 1
+        sel_ratio = np.exp( np.log(max_sel/min_sel) / (resolution-1) )
+        sel_range = [(min_sel*sel_ratio**i) for i in range(resolution)]
+        if zero_sel:
+            sel_range.insert( 0, 0 )
+    else:
+        sel_diff = max_sel / (resolution-1)
+        sel_range = [ i*sel_diff for i in range(resolution)]
+    sel_range = np.array( sel_range )
+    if sel_round is not None:
+        sel_range = np.round( sel_range, sel_round )
+
+    'Step 0: Find cost values for each iso-cost surface'
+    '''
+    '''
+
+    'Step 1: Find Plans on each Iso-cost surface'
+    '''
+    '''
+    nexus()
+
+    'Step 2: Reduing overall number of plans, effectively reducing plan density on each iso-cost surface'
+    '''
+    '''
+    if anorexic:
+        anorexic_reduction()
+
+    'Step 3: Find Plans on each Iso-cost surface'
+    '''
+    '''
+    if covering:
+        covering_sequence()
+
+    'Step 4: Simulating Plan-Bouquet Execution under Idea Cost model assumption'
+    '''
+    '''
+    simulate()
+
+
+
