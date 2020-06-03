3686d98bcafcf2ff9f9cf717ba88dc43b8ee7bfd
chaudharyachint08 <chaudharyachint08@gmail.com>
2020-06-03 22:49:50 +0530

    Violin Deviation

---
+++ source/violinplots.py
@@ -0,0 +1,10 @@
+import numpy as np, pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+
+sns.violinplot(x=None, y=None, hue=None, data=None, order=None, hue_order=None, bw='scott', cut=2, scale='area', scale_hue=True, gridsize=100, width=0.8, inner='quartile', split=False, dodge=True, orient=None, linewidth=None, color=None, palette=None, saturation=0.75, ax=None, **kwargs)
+
+plt.plot
