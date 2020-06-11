b12495a39f4957e3ec42861c10531273c5ee2652
chaudharyachint08 <chaudharyachint08@gmail.com>
2020-06-12 03:02:01 +0530

    Weight Calculation for Tuning Direction Vector in ESS Space

---
+++ source/exponential_movement.py
@@ -0,0 +1,27 @@
+import numpy as np, pandas as pd
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def f(N,r=0.5):
+	return (1-(1-r)**N)
+
+X_pow = 6
+for r in np.linspace(0.1,0.9,9):
+	X = [2**n for n in range(X_pow)]
+	Y = [f(x,r) for x in X]
+	_ = plt.plot( X , Y , label='{:0.3f}'.format(r) )
+
+plt.xscale('log',basex=2)
+# plt.yscale('log',basey=2)
+
+plt.xlabel('Step Size')
+plt.ylabel('Exponential Weight')
+
+plt.title('Weight Calculation for Tuning Direction Vector in ESS Space')
+
+plt.grid(True, which='both')
+
+
+plt.legend()
+plt.show()
